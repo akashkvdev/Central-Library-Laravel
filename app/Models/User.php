60e69c3ef00f0c1c56eeef49362e15fe05ec9ce6
hmnteamproject <hmnteam2023@gmail.com>
2023-12-23 14:05:31 +0530

    Central-Library-Laravel

---
+++ app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends Authenticatable
+{
+    use HasApiTokens,HasFactory;
+    protected $table="users";
+    protected $primaryKey="user_id";
+}
